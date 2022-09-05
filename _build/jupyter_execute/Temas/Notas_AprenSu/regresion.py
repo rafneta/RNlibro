adf75a8cdf8d6bd7929849d27f094371521821db
rafaMac-Air <raf_neta@hotmail.com>
2022-09-05 01:43:09 -0500

    otoño  2022

---
+++ _build/jupyter_execute/Temas/Notas_AprenSu/regresion.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ![](https://raw.githubusercontent.com/rafneta/RNlibro/master/imagenes/banner.png)
+# 
+# ```{contents}
+# :depth: 4
+# ```
+# 
+# # Regresión
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
